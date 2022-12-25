81124d4566670cdc0aafb194cea1c41cb93ecee3
Samuel Freitas <samuelfreitas.ao@gmail.com>
2022-12-25 13:31:20 +0100

    test: ensure MinLengthVAlidation returns falsy if value is valid

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -7,4 +7,10 @@ describe('MinLengthVAlidation', () => {
     const error = sut.validate('123')
     expect(error).toEqual(new InvalidFieldError())
   })
+
+  test('Should return falsy if value is valid', () => {
+    const sut = new MinLengthVAlidation('field', 5)
+    const error = sut.validate('12345')
+    expect(error).toBeFalsy()
+  })
 })
