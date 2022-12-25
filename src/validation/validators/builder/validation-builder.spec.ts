2f49fc064ad5979209fb66b62526f1cbe79defb9
Samuel Freitas <samuelfreitas.ao@gmail.com>
2022-12-25 20:13:11 +0100

    test: ensure ValidatioBuilder returns a list of validations

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -5,19 +5,40 @@ import {
   ValidationBuilder,
 } from '@/validation/validators'
 
+import faker from 'faker'
+
 describe('ValidationBuilder', () => {
   test('Should return RequiredFieldValidation', () => {
-    const validations = ValidationBuilder.field('field').required().build()
-    expect(validations).toEqual([new RequiredFieldValidation('field')])
+    const field = faker.database.column()
+    const validations = ValidationBuilder.field(field).required().build()
+    expect(validations).toEqual([new RequiredFieldValidation(field)])
   })
 
   test('Should return EmailValidation', () => {
-    const validations = ValidationBuilder.field('field').email().build()
-    expect(validations).toEqual([new EmailValidation('field')])
+    const field = faker.database.column()
+    const validations = ValidationBuilder.field(field).email().build()
+    expect(validations).toEqual([new EmailValidation(field)])
   })
 
   test('Should return MinLengthVAlidation', () => {
-    const validations = ValidationBuilder.field('field').min(5).build()
-    expect(validations).toEqual([new MinLengthVAlidation('field', 5)])
+    const field = faker.database.column()
+    const length = faker.datatype.number()
+    const validations = ValidationBuilder.field(field).min(length).build()
+    expect(validations).toEqual([new MinLengthVAlidation(field, length)])
+  })
+
+  test('Should return a list of validations', () => {
+    const field = faker.database.column()
+    const length = faker.datatype.number()
+    const validations = ValidationBuilder.field(field)
+      .required()
+      .email()
+      .min(length)
+      .build()
+    expect(validations).toEqual([
+      new RequiredFieldValidation(field),
+      new EmailValidation(field),
+      new MinLengthVAlidation(field, length),
+    ])
   })
 })
